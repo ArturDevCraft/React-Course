881d7391beb59e7651fb72bc32de20a08f43c662
Artur <artur.czerwiak@gmail.com>
2025-01-19 16:44:17 +0100

    Add automatic logout

--- Project 23 - Authentication in REACT app/frontend/src/pages/Root.js
+++ Project 23 - Authentication in REACT app/frontend/src/pages/Root.js
@@ -1,19 +1,36 @@
-import { Outlet, useNavigation } from 'react-router-dom';
+import {
+	Outlet,
+	useLoaderData,
+	useNavigation,
+	useSubmit,
+} from 'react-router-dom';
 
 import MainNavigation from '../components/MainNavigation';
+import { useEffect } from 'react';
 
 function RootLayout() {
-  // const navigation = useNavigation();
+	const token = useLoaderData();
+	const submit = useSubmit();
+	// const navigation = useNavigation();
+	useEffect(() => {
+		if (!token) {
+			return;
+		}
 
-  return (
-    <>
-      <MainNavigation />
-      <main>
-        {/* {navigation.state === 'loading' && <p>Loading...</p>} */}
-        <Outlet />
-      </main>
-    </>
-  );
+		setTimeout(() => {
+			submit(null, { action: '/logout', method: 'post' });
+		}, 1 * 60 * 60 * 1000);
+	}, [token, submit]);
+
+	return (
+		<>
+			<MainNavigation />
+			<main>
+				{/* {navigation.state === 'loading' && <p>Loading...</p>} */}
+				<Outlet />
+			</main>
+		</>
+	);
 }
 
 export default RootLayout;
