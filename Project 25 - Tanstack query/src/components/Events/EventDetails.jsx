36fc19499479324335353c810bb18f1631552b69
Artur <artur.czerwiak@gmail.com>
2025-01-25 21:06:17 +0100

    Add deleting single event functionality

--- Project 25 - Tanstack query/src/components/Events/EventDetails.jsx
+++ Project 25 - Tanstack query/src/components/Events/EventDetails.jsx
@@ -1,60 +1,35 @@
-import { useQuery } from '@tanstack/react-query';
-import { Link, Outlet, useParams } from 'react-router-dom';
+import { useMutation, useQuery } from '@tanstack/react-query';
+import { Link, Outlet, useNavigate, useParams } from 'react-router-dom';
 
 import Header from '../Header.jsx';
-import { fetchEvent } from '../../util/http.js';
+import { deleteEvent, fetchEvent, queryClient } from '../../util/http.js';
 import LoadingIndicator from '../UI/LoadingIndicator.jsx';
 import ErrorBlock from '../UI/ErrorBlock.jsx';
 
 export default function EventDetails() {
 	const { id } = useParams();
+	const navigate = useNavigate();
+
+	const {
+		mutate,
+		isPending: isDeletePending,
+		isError: isDeleteError,
+		error: deleteError,
+	} = useMutation({
+		mutationFn: () => deleteEvent({ id }),
+		onSuccess: () => {
+			queryClient.invalidateQueries({ queryKey: ['events'] });
+			navigate('/events');
+		},
+	});
 
 	const { data, isPending, isError, error } = useQuery({
 		queryKey: ['event-' + id],
 		queryFn: ({ signal }) => fetchEvent({ id, signal }),
 	});
 
-	function handleDelete() {}
-
-	let content;
-
-	if (isPending) {
-		content = <LoadingIndicator />;
-	}
-
-	if (isError) {
-		content = (
-			<ErrorBlock
-				title="An error occured"
-				message={error.info?.message || 'Failed to fetch event details data'}
-			/>
-		);
-	}
-
-	if (data) {
-		content = (
-			<article id="event-details">
-				<header>
-					<h1>{data.title}</h1>
-					<nav>
-						<button>Delete</button>
-						<Link to="edit">Edit</Link>
-					</nav>
-				</header>
-				<div id="event-details-content">
-					<img src={`http://localhost:3000/${data.image}`} alt={data.title} />
-					<div id="event-details-info">
-						<div>
-							<p id="event-details-location">{data.location}</p>
-							<time dateTime={`${data.date}$${data.time}`}>
-								{data.date} {data.time}
-							</time>
-						</div>
-						<p id="event-details-description">{data.description}</p>
-					</div>
-				</div>
-			</article>
-		);
+	function handleDelete() {
+		mutate();
 	}
 
 	return (
@@ -65,7 +40,47 @@ export default function EventDetails() {
 					View all Events
 				</Link>
 			</Header>
-			{content}
+			{isPending && <LoadingIndicator />}
+			{isDeletePending && <LoadingIndicator />}
+
+			{isError && (
+				<ErrorBlock
+					title="An error occured"
+					message={error.info?.message || 'Failed to fetch event details data.'}
+				/>
+			)}
+
+			{isDeleteError && (
+				<ErrorBlock
+					title="An error occured"
+					message={deleteError.info?.message || 'Failed to delete event.'}
+				/>
+			)}
+			{data && (
+				<article id="event-details">
+					<header>
+						<h1>{data.title}</h1>
+						<nav>
+							<button onClick={handleDelete} disabled={isPending}>
+								{isDeletePending ? 'Deleting...' : 'Delete'}
+							</button>
+							<Link to="edit">Edit</Link>
+						</nav>
+					</header>
+					<div id="event-details-content">
+						<img src={`http://localhost:3000/${data.image}`} alt={data.title} />
+						<div id="event-details-info">
+							<div>
+								<p id="event-details-location">{data.location}</p>
+								<time dateTime={`${data.date}$${data.time}`}>
+									{data.date} {data.time}
+								</time>
+							</div>
+							<p id="event-details-description">{data.description}</p>
+						</div>
+					</div>
+				</article>
+			)}
 		</>
 	);
 }
