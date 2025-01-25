942da0c2509e1edbab6022a15d840cde4cb38aa7
Artur <artur.czerwiak@gmail.com>
2025-01-25 20:09:52 +0100

    Add fetching event details data in EventDetail component

--- Project 25 - Tanstack query/src/components/Events/EventDetails.jsx
+++ Project 25 - Tanstack query/src/components/Events/EventDetails.jsx
@@ -1,35 +1,46 @@
-import { Link, Outlet } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import { Link, Outlet, useParams } from 'react-router-dom';
 
 import Header from '../Header.jsx';
+import { fetchEvent } from '../../util/http.js';
 
 export default function EventDetails() {
-  return (
-    <>
-      <Outlet />
-      <Header>
-        <Link to="/events" className="nav-item">
-          View all Events
-        </Link>
-      </Header>
-      <article id="event-details">
-        <header>
-          <h1>EVENT TITLE</h1>
-          <nav>
-            <button>Delete</button>
-            <Link to="edit">Edit</Link>
-          </nav>
-        </header>
-        <div id="event-details-content">
-          <img src="" alt="" />
-          <div id="event-details-info">
-            <div>
-              <p id="event-details-location">EVENT LOCATION</p>
-              <time dateTime={`Todo-DateT$Todo-Time`}>DATE @ TIME</time>
-            </div>
-            <p id="event-details-description">EVENT DESCRIPTION</p>
-          </div>
-        </div>
-      </article>
-    </>
-  );
+	const { id } = useParams();
+
+	const { data, isPending, isError, error } = useQuery({
+		queryKey: ['event-' + id],
+		queryFn: ({ signal }) => fetchEvent({ id, signal }),
+	});
+	console.log(data);
+	return (
+		<>
+			<Outlet />
+			<Header>
+				<Link to="/events" className="nav-item">
+					View all Events
+				</Link>
+			</Header>
+			<article id="event-details">
+				<header>
+					<h1>{data.title}</h1>
+					<nav>
+						<button>Delete</button>
+						<Link to="edit">Edit</Link>
+					</nav>
+				</header>
+				<div id="event-details-content">
+					<img src={`http://localhost:3000/${data.image}`} alt={data.title} />
+					<div id="event-details-info">
+						<div>
+							<p id="event-details-location">{data.location}</p>
+							<time dateTime={`${data.date}$${data.time}`}>
+								{data.date} {data.time}
+							</time>
+						</div>
+						<p id="event-details-description">{data.description}</p>
+					</div>
+				</div>
+			</article>
+		</>
+	);
 }
