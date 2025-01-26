f87704f1dea6c05683d12626daf68199d77ad2e4
Artur <artur.czerwiak@gmail.com>
2025-01-26 20:43:12 +0100

    Add passing queryKey to query function and limit list elements

--- Project 25 - Tanstack query/src/components/Events/NewEventsSection.jsx
+++ Project 25 - Tanstack query/src/components/Events/NewEventsSection.jsx
@@ -6,8 +6,8 @@ import { fetchEvents } from '../../util/http.js';
 
 export default function NewEventsSection() {
 	const { data, isPending, isError, error } = useQuery({
-		queryKey: ['events'],
-		queryFn: fetchEvents,
+		queryKey: ['events', { max: 3 }],
+		queryFn: ({ signal, queryKey }) => fetchEvents({ signal, ...queryKey[1] }),
 		staleTime: 5000, //when data are re-rendered after this time - tanstack sends new request - if not it use cached data without checking it by request
 		//gcTime: 30000, //  how long data is kept around in cache
 	});
