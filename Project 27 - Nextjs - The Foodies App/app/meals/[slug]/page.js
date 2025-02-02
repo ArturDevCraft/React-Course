3812aa604cfdbcc05fdfc856343573fd2a579bda
Artur <artur.czerwiak@gmail.com>
2025-02-02 15:41:48 +0100

    Add metadata and dynamicly loaded metadata for different pages

--- Project 27 - Nextjs - The Foodies App/app/meals/[slug]/page.js
+++ Project 27 - Nextjs - The Foodies App/app/meals/[slug]/page.js
@@ -5,6 +5,17 @@ import { notFound } from 'next/navigation';
 import classes from './page.module.css';
 import { getMeal } from '@/lib/meals';
 
+export async function generateMetadata({ params }) {
+	const meal = getMeal(params.slug);
+	if (!meal) {
+		notFound();
+	}
+	return {
+		title: meal.title,
+		description: meal.summary,
+	};
+}
+
 export default function MealDetailPage({ params }) {
 	const meal = getMeal(params.slug);
 
