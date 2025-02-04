c77a9f204e622d5d28d8668fb706de8006310268
Artur <artur.czerwiak@gmail.com>
2025-02-04 22:53:26 +0100

    Add generating pre-renderd static data and paths for dynamic page addresses

--- Project 29 - Nextjs - The Meetup App/pages/[meetupId]/index.js
+++ Project 29 - Nextjs - The Meetup App/pages/[meetupId]/index.js
@@ -11,5 +11,41 @@ function MeetupDetails() {
 		/>
 	);
 }
+export async function getStaticPaths() {
+	return {
+		fallback: false,
+		paths: [
+			{
+				params: {
+					meetupId: 'm1',
+				},
+			},
+			{
+				params: {
+					meetupId: 'm2',
+				},
+			},
+		],
+	};
+}
+
+export function getStaticProps(context) {
+	//fetch data for a single meetup
+	const meetupId = context.params.meetupId;
+	console.log(meetupId);
+
+	return {
+		props: {
+			meetupData: {
+				image:
+					'https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg',
+				id: meetupId,
+				title: 'A first meetup',
+				address: 'Some address 5, 12345 Some City',
+				description: 'This is a first meetup!',
+			},
+		},
+	};
+}
 
 export default MeetupDetails;
