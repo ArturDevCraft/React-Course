8e51ba7c1c8ba65e7ef64473ba4d97342841da09
Artur <artur.czerwiak@gmail.com>
2025-02-08 10:37:55 +0100

    Get rid redundand code

--- Project 29 - Nextjs - The Meetup App/pages/index.js
+++ Project 29 - Nextjs - The Meetup App/pages/index.js
@@ -2,32 +2,6 @@ import { MongoClient } from 'mongodb';
 
 import MeetupList from '../components/meetups/MeetupList';
 
-const DUMMY_MEETUPS = [
-	{
-		id: 'm1',
-		title: 'A first meetup',
-		image:
-			'https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg',
-		address: 'Some address 5, 12345 Some City',
-		description: 'This is a first meetup!',
-	},
-	{
-		id: 'm2',
-		title: 'A second meetup',
-		image:
-			'https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg',
-		address: 'Some address 15, 12345 Some City',
-		description: 'This is a second meetup!',
-	},
-	{
-		id: 'm3',
-		title: 'A third meetup',
-		image:
-			'https://upload.wikimedia.org/wikipedia/commons/d/d3/Stadtbild_M%C3%BCnchen.jpg',
-		address: 'Some address 25, 12345 Some City',
-		description: 'This is a third meetup!',
-	},
-];
 function HomePage(props) {
 	return <MeetupList meetups={props.meetups} />;
 }
