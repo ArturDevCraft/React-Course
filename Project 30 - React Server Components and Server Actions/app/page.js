c0c21c11dc612c411b747711c230643e0c88b00f
Artur <artur.czerwiak@gmail.com>
2025-02-08 18:19:52 +0100

    Add error boundary component

--- Project 30 - React Server Components and Server Actions/app/page.js
+++ Project 30 - React Server Components and Server Actions/app/page.js
@@ -1,20 +1,24 @@
 import UsePromiseDemo from '@/Components/UsePromisesDemo';
 import { Suspense } from 'react';
 import fs from 'node:fs/promises';
+import ErrorBoundary from '@/Components/ErrorBoundary';
 
 export default async function Home() {
-	const fetchUserPromise = new Promise((resolve) =>
+	const fetchUserPromise = new Promise((resolve, reject) =>
 		setTimeout(async () => {
 			const data = await fs.readFile('dummy-db.json', 'utf-8');
 			const users = JSON.parse(data);
-			resolve(users);
+			// resolve(users);
+			reject(new Error('Error'));
 		}, 2000)
 	);
 	return (
 		<main>
-			<Suspense fallback={<p>Loading...</p>}>
-				<UsePromiseDemo userPromise={fetchUserPromise} />
-			</Suspense>
+			<ErrorBoundary fallback={<p>Something went wrong!</p>}>
+				<Suspense fallback={<p>Loading...</p>}>
+					<UsePromiseDemo userPromise={fetchUserPromise} />
+				</Suspense>
+			</ErrorBoundary>
 		</main>
 	);
 }
