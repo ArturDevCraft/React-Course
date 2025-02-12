17478f8e3704639b3f1adbe28c0ab55f8ba4a989
Artur <artur.czerwiak@gmail.com>
2025-02-12 18:07:34 +0100

    Add Contex in AccordionItem component to share id prop with nested coomponents

--- Project 32 - React Patterns and Best Practices/src/App.jsx
+++ Project 32 - React Patterns and Best Practices/src/App.jsx
@@ -6,14 +6,11 @@ function App() {
 			<section>
 				<h2>Why work with us?</h2>
 				<Accordion className="accordion">
-					<Accordion.Item className="accordion-item">
-						<Accordion.Title id="experience" className="accordion-item-title">
+					<Accordion.Item id="experience" className="accordion-item">
+						<Accordion.Title className="accordion-item-title">
 							We got 20 year of experience
 						</Accordion.Title>
-						<Accordion.Content
-							className="accordion-item-content"
-							id="experience"
-						>
+						<Accordion.Content className="accordion-item-content">
 							<article>
 								<p>You can&apos;t go wrong with us.</p>
 								<p>
@@ -23,22 +20,20 @@ function App() {
 							</article>
 						</Accordion.Content>
 					</Accordion.Item>
-
-					<Accordion.Title id="local-guide" className="accordion-item-title">
-						We're working with local guides
-					</Accordion.Title>
-					<Accordion.Content
-						className="accordion-item-content"
-						id="local-guide"
-					>
-						<article>
-							<p>We are not doing this along from our office.</p>
-							<p>
-								Instead, we are working with local guides to ensure a safe and
-								pleasant vacation.
-							</p>
-						</article>
-					</Accordion.Content>
+					<Accordion.Item id="local-guide" className="accordion-item">
+						<Accordion.Title className="accordion-item-title">
+							We're working with local guides
+						</Accordion.Title>
+						<Accordion.Content className="accordion-item-content">
+							<article>
+								<p>We are not doing this along from our office.</p>
+								<p>
+									Instead, we are working with local guides to ensure a safe and
+									pleasant vacation.
+								</p>
+							</article>
+						</Accordion.Content>
+					</Accordion.Item>
 				</Accordion>
 			</section>
 		</main>
