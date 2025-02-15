c4903671c43b108a1bd4cbd343598d1f23eebe33
Artur <artur.czerwiak@gmail.com>
2025-02-15 11:05:43 +0100

    Display favorites products with context instead of redux

--- Project 33 - Replacing Redux with React Hooks/src/index.js
+++ Project 33 - Replacing Redux with React Hooks/src/index.js
@@ -4,7 +4,6 @@ import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
-import productReducer from './store/reducers/products';
 import ProductsProvider from './context/products-context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
