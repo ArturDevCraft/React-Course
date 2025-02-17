8f548f1cc2909cb79efe2c60aa586e7c4042da99
Artur <artur.czerwiak@gmail.com>
2025-02-17 16:48:30 +0100

    Add tests for changes after button clicked

--- Project 34 - Testing React Apps - Unit Tests/src/components/Greeting.test.js
+++ Project 34 - Testing React Apps - Unit Tests/src/components/Greeting.test.js
@@ -1,4 +1,6 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
 import Greeting from './Greeting';
 
 describe('Greeting component', () => {
@@ -12,5 +14,36 @@ describe('Greeting component', () => {
 		//Assert
 		const helloWorldElement = screen.getByText('Hello World', { exact: false });
 		expect(helloWorldElement).toBeInTheDocument();
+	});
+
+	test("render It'is good to see you if the button was NOT clicked", () => {
+		render(<Greeting />);
+
+		const seeYouElement = screen.getByText("It'is good to see you", {
+			exact: false,
+		});
+		expect(seeYouElement).toBeInTheDocument();
+	});
+
+	test('renders "Cahnged!" if the button was clicked', () => {
+		render(<Greeting />);
+		const buttonElement = screen.getByRole('button');
+		userEvent.click(buttonElement);
+
+		const changedElement = screen.getByText('changed', { exact: false });
+
+		expect(changedElement).toBeInTheDocument();
+	});
+
+	test('does NOT render "good to see you!" if the button was clicked', () => {
+		render(<Greeting />);
+		const buttonElement = screen.getByRole('button');
+		userEvent.click(buttonElement);
+
+		const seeYouElement = screen.queryByText("It'is good to see you", {
+			exact: false,
+		});
+
+		expect(seeYouElement).toBeNull();
 	});
 });
