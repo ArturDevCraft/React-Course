1e924fef75f934872459b6f5db065c8833bbae6a
Artur <artur.czerwiak@gmail.com>
2025-02-18 17:49:54 +0100

    Description of types in Typescript

---
+++ Project 35 - Typescript basics/basics.ts
@@ -0,0 +1,38 @@
+// Primitives: number, string, boolean
+// More complex types: arrays, objects
+// Function types, parameters
+
+// Primitives
+
+let age: number = 24;
+
+age = 12;
+
+let userName: string;
+
+userName = 'Artur';
+
+let isInstructor: boolean;
+
+isInstructor = false;
+
+// More complex types
+
+let hobbies: string[];
+
+hobbies = ['Sport', 'Cooking'];
+
+let person: {
+	name: string;
+	age: number;
+};
+
+person = {
+	name: 'Artur',
+	age: 35,
+};
+
+let people: {
+	name: string;
+	age: number;
+}[];
