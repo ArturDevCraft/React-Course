3943da1feae35577667bf03c106853df97e847c3
Artur <artur.czerwiak@gmail.com>
2025-02-19 18:00:49 +0100

    Define types for React component props

--- Project 36 - Typesricpt with React/my-app/src/App.tsx
+++ Project 36 - Typesricpt with React/my-app/src/App.tsx
@@ -1,9 +1,12 @@
 import './App.css';
+import Todos from './components/Todos';
 
 function App() {
-	return <div className="App">
-    
-  </div>;
+	return (
+		<div>
+			<Todos items={['Learn React', 'Learn TypeScript']} />
+		</div>
+	);
 }
 
 export default App;
