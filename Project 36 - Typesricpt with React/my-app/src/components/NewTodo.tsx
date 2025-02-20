93b3b9330e6dc06385d42b4fb65e7c7db088ec96
Artur <artur.czerwiak@gmail.com>
2025-02-20 18:08:33 +0100

    Add css styles

--- Project 36 - Typesricpt with React/my-app/src/components/NewTodo.tsx
+++ Project 36 - Typesricpt with React/my-app/src/components/NewTodo.tsx
@@ -1,5 +1,6 @@
 import { text } from 'node:stream/consumers';
 import { useRef } from 'react';
+import classes from './NewTodo.module.css';
 
 const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
 	const textInputRef = useRef<HTMLInputElement>(null);
@@ -19,7 +20,7 @@ const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
 		}
 	};
 	return (
-		<form onSubmit={submitHandler}>
+		<form onSubmit={submitHandler} className={classes.form}>
 			<label htmlFor="text">Todo text</label>
 			<input type="text" id="text" ref={textInputRef} />
 			<button>Add Todo</button>
