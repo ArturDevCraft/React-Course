a860d3a1e9cee8b09340c342962976951e1334e6
Artur <artur.czerwiak@gmail.com>
2025-03-01 17:16:49 +0100

    Update project 25 for deployment

--- Project 25 - Tanstack query/src/components/Events/EventDetails.jsx
+++ Project 25 - Tanstack query/src/components/Events/EventDetails.jsx
@@ -105,7 +105,10 @@ export default function EventDetails() {
 						</nav>
 					</header>
 					<div id="event-details-content">
-						<img src={`http://localhost:3000/${data.image}`} alt={data.title} />
+						<img
+							src={`https://react-course-project-25.onrender.com/${data.image}`}
+							alt={data.title}
+						/>
 						<div id="event-details-info">
 							<div>
 								<p id="event-details-location">{data.location}</p>
